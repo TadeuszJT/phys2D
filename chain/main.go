7d48a7fa10213c8761fe6bb09926f04ea18e0b6b
Tadeusz Tomoszek <tadeuszjt@protonmail.com>
2022-09-25 17:13:39 +0100

    improvements to chain

--- chain/main.go
+++ chain/main.go
@@ -2,49 +2,40 @@ package main
 
 import (
     "fmt"
-	geom "github.com/tadeuszjt/geom/64"
+	"github.com/tadeuszjt/geom/64"
 	"github.com/tadeuszjt/phys2D"
     "github.com/tadeuszjt/gfx"
 
-    newGeom "github.com/tadeuszjt/geom/32"
+    geom32 "github.com/tadeuszjt/geom/32"
 )
 
 var (
 	world phys2D.World
-    rectKeys []phys2D.Key
-
     rectSize = geom.Rect{geom.Vec2{-10, -10}, geom.Vec2{10, 10}}
-    rectStartPositions = []geom.Ori2 {
-        {100, 60, 0},
-        {120, 80, 0},
-        {140, 100, 0},
-        {160, 120, 0},
-        {180, 140, 0},
-        {200, 160, 0},
-        {220, 180, 0},
-    }
+    rectKeys []phys2D.Key
+    numRects = 7
 )
 
 func setup(w *gfx.Win) error {
-
-    for i, p := range rectStartPositions {
+    for i := 0; i < numRects; i++ {
         mass := phys2D.MassRectangle(rectSize)
-
         if i == 0 {
             mass = geom.Ori2{0, 0, 0}
         }
 
-        rectKeys = append(rectKeys, world.AddBody(p, mass))
+        ori := geom.Ori2 {
+            300 + float64(i) * 20,
+            60 + float64(i)*20,
+            0,
+        }
+
+        rectKeys = append(rectKeys, world.AddBody(ori, mass))
+        if i > 0 {
+            world.AddJoint(rectKeys[i-1], rectKeys[i], geom.Vec2{10, 10}, geom.Vec2{-10, -10})
+        }
     }
 
     world.Gravity = geom.Ori2{0, 10, 0}
-
-    world.AddJoint(rectKeys[0], rectKeys[1], geom.Vec2{10, 10}, geom.Vec2{-10, -10})
-    world.AddJoint(rectKeys[1], rectKeys[2], geom.Vec2{10, 10}, geom.Vec2{-10, -10})
-    world.AddJoint(rectKeys[2], rectKeys[3], geom.Vec2{10, 10}, geom.Vec2{-10, -10})
-    world.AddJoint(rectKeys[3], rectKeys[4], geom.Vec2{10, 10}, geom.Vec2{-10, -10})
-    world.AddJoint(rectKeys[4], rectKeys[5], geom.Vec2{10, 10}, geom.Vec2{-10, -10})
-    world.AddJoint(rectKeys[5], rectKeys[6], geom.Vec2{10, 10}, geom.Vec2{-10, -10})
 
     return nil
 }
@@ -53,18 +44,31 @@ func setup(w *gfx.Win) error {
 func draw(w* gfx.Win, c gfx.Canvas) {
     oris := world.GetOrientations(rectKeys)
 
-    orisConverted := []newGeom.Ori2{}
-
+    orisConverted := []geom32.Ori2{}
     for _, o := range oris {
-        orisConverted = append(orisConverted, newGeom.Ori2{float32(o.X), float32(o.Y), newGeom.Angle(o.Theta)})
+        orisConverted = append(orisConverted, geom32.Ori2{float32(o.X), float32(o.Y), geom32.Angle(o.Theta)})
     }
 
     for i := range orisConverted {
-        gfx.DrawSprite(c, orisConverted[i], newGeom.RectCentred(20, 20), gfx.Red, nil, nil)
+        gfx.DrawSprite(c, orisConverted[i], geom32.RectCentred(20, 20), gfx.Red, nil, nil)
     }
 
-    world.Update(0.1)
-    fmt.Println("benis")
+    world.Update(1 / 60.) // 60 fps
+    world.Update(1 / 60.) // 60 fps
+    world.Update(1 / 60.) // 60 fps
+    world.Update(1 / 60.) // 60 fps
+    world.Update(1 / 60.) // 60 fps
+    world.Update(1 / 60.) // 60 fps
+    world.Update(1 / 60.) // 60 fps
+    world.Update(1 / 60.) // 60 fps
+    world.Update(1 / 60.) // 60 fps
+    world.Update(1 / 60.) // 60 fps
+    world.Update(1 / 60.) // 60 fps
+    world.Update(1 / 60.) // 60 fps
+    world.Update(1 / 60.) // 60 fps
+    world.Update(1 / 60.) // 60 fps
+    world.Update(1 / 60.) // 60 fps
+    world.Update(1 / 60.) // 60 fps
 }
 
 func main() {
